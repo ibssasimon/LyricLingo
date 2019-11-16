c3f6a8e447b353a60900a4b93afd5f03cd359a6c
Simon Ibssa <ibssasimon@gmail.com>
2019-11-16 13:42:13 -0800

    hey

--- hello_flask/lyriclingoweb.py
+++ hello_flask/lyriclingoweb.py
@@ -11,14 +11,21 @@ with open("../translated_lyrics.txt", "r") as f:
     songs = []
     songs = content.split("*[")
     songs.remove('')
-
+    
     titles = []
+    for song in songs:
+        x = song.split("\n")
+        titles.append(x[0])
+
+    for i in range(len(titles)):
+        titles[i] = titles[i][:-1]
 
 @APP.route("/")
 def index():
     """ Displays the index page accessible at '/'
     """
-    return render_template("test.html", content=songs[1])
+    return render_template("test.html", content=titles)
+    
 
 
 if __name__ == '__main__':
