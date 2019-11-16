a7f8796053fe17312606db1a4885e4aa11fe6e84
Simon Ibssa <ibssasimon@gmail.com>
2019-11-16 13:27:37 -0800

    songs are in

--- hello_flask/lyriclingoweb.py
+++ hello_flask/lyriclingoweb.py
@@ -6,12 +6,19 @@ from flask import Flask, render_template
 # Create the application.
 APP = Flask(__name__)
 
+with open("../translated_lyrics.txt", "r") as f:
+    content = f.read()
+    songs = []
+    songs = content.split("*[")
+    songs.remove('')
+
+    titles = []
 
 @APP.route("/")
 def index():
     """ Displays the index page accessible at '/'
     """
-    return render_template("index.html")
+    return render_template("test.html", content=songs[1])
 
 
 if __name__ == '__main__':
